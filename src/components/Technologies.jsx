aeef55c52bd269b74968b0a49701157cf12fb77e
Rudra Gowda H <121507406+Rudragowdah@users.noreply.github.com>
2024-12-02 13:12:50 +0530

    About Page Implementation

---
+++ src/components/Technologies.jsx
@@ -0,0 +1,16 @@
+// import { IoLogoCss3 } from "react-icons/io";
+// import { SiJavascript } from "react-icons/si";
+// import { FaHtml5 } from "react-icons/fa";
+// import { RiReactjsLine } from "react-icons/ri";
+// import { BiLogoMongodb } from "react-icons/bi";
+// import { TbBrandNodejs } from "react-icons/tb";
+
+const Technologies = () => {
+  return (
+    <div>
+      <h1 className="my-20 text-4xl text-center">Technologies</h1>
+    </div>
+  )
+}
+
+export default Technologies
